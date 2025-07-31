638a010809db75de9d4c93d07722f4107e2d62e8
Perpetual Nwadiugwu <157845591+naecherem20@users.noreply.github.com>
2025-07-31 13:09:09 +0100

    Revert "Feature: User Registration"

--- schema/user.py
+++ schema/user.py
@@ -5,16 +5,4 @@ from pydantic import  EmailStr
 class User_create(BaseModel):
     username: str
     email: EmailStr
-
     password: str
-
-class User_read(BaseModel):
-    id: int
-    username: str
-    email: EmailStr
-
-    class Config:    
-        orm_mode = True
-        
-    password: str
-
